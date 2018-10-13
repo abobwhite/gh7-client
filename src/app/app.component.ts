d201439ddcd368be5e1a67d1c510909433483ee7
Alex White <alex.white@daugherty.com>
2018-10-13 00:09:31 -0500

    Add code from a starter using Auth-0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'gh7-client-test';
-  status: Observable<string>;
-
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-    this.status = this.http.get<string>('/api/actuator/health').pipe(map((info: any) => info.status));
+export class AppComponent {
+  constructor(public auth: AuthService) {
+    auth.handleAuthentication();
   }
 }
