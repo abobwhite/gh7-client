6774bcf546d2ebb518ae88474f5c280a3dbc756d
Alex White <alex.white@daugherty.com>
2018-10-13 11:55:11 -0500

    Add production env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
+  AUTHO_CLIENT_ID: 'r2xf6jVZqYIDMnzn2zFoyKz8aw61yp8K',
+  AUTH0_DOMAIN: 'gh7-beacon.auth0.com',
   AUTH0_REDIRECT_URL: 'http://167.99.0.129/'
 };
