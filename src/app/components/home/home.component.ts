af3338f3fc0326270ea91f95b772f8751d12bc4e
Alex White <alex.white@daugherty.com>
2018-10-14 05:39:06 -0500

    Homepage!

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {faGlobeAfrica} from '@fortawesome/free-solid-svg-icons';
+import {faUserSecret} from '@fortawesome/free-solid-svg-icons';
+import {faAmbulance} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-welcome',
@@ -7,6 +10,9 @@ import {AuthService} from '../../services/auth.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  faGlobeAfrica = faGlobeAfrica;
+  faUserSecret = faUserSecret;
+  faAmbulance = faAmbulance;
 
   constructor(private auth: AuthService) {
 
