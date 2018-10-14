025f6279c83bc325a824136ce19497181137ffb5
Alex White <alex.white@daugherty.com>
2018-10-13 23:10:48 -0500

    Improve assistance dashboard UX

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {Observable} from 'rxjs';
 import {User} from '../../models/User';
-import {faAmbulance, faHandsHelping, faUserShield} from '@fortawesome/free-solid-svg-icons';
+import {faAmbulance, faHandsHelping, faUserShield, faLifeRing} from '@fortawesome/free-solid-svg-icons';
 import {UserAssistanceService} from '../../services/userAssistance.service';
 import {UserAssistanceRequest} from '../../models/UserAssistanceRequest';
 import {ASSISTANCE_CAPABILITY} from '../../models/ASSISTANCE_CAPABILITY';
@@ -17,6 +17,7 @@ export class DashboardComponent implements OnInit {
   faHandsHelping = faHandsHelping;
   faAmbulance = faAmbulance;
   faUserShield = faUserShield;
+  faLifeRing = faLifeRing;
   user$: Observable<User>;
 
   constructor(private auth: AuthService, private snackBar: MatSnackBar, private userAssistanceService: UserAssistanceService) {
