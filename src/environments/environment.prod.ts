186d3919030aff54b5a1ca54d5e7f79810f977ec
Alex White <alex.white@daugherty.com>
2019-02-21 16:28:49 -0600

    Change Auth0 redirect URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   AUTH0_AUDIENCE: 'gh7-beacon-api',
   AUTHO_CLIENT_ID: 'r2xf6jVZqYIDMnzn2zFoyKz8aw61yp8K',
   AUTH0_DOMAIN: 'gh7-beacon.auth0.com',
-  AUTH0_REDIRECT_URL: 'http://167.99.0.129/'
+  AUTH0_REDIRECT_URL: 'http://68.183.122.97/'
 };
